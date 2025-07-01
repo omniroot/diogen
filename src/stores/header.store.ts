935ea5ef300ea3d75532b4989470e20bea14b55c
omniroot <omnirootofc@gmail.com>
2025-07-01 17:46:24 +0300

    Update useHeader store

--- src/stores/header.store.ts
+++ src/stores/header.store.ts
@@ -1,12 +1,15 @@
 import { create } from "zustand";
 
 interface IStore {
-	isCollapsed: boolean;
-	toggleIsCollapsed: () => void;
+  isCollapsed: boolean;
+  toggleIsCollapsed: (newState?: boolean) => void;
 }
 
 export const useHeader = create<IStore>((set) => ({
-	isCollapsed: false,
-	toggleIsCollapsed: () =>
-		set((state) => ({ isCollapsed: !state.isCollapsed })),
+  isCollapsed: false,
+  toggleIsCollapsed: (newState = undefined) => {
+    set((state) => ({
+      isCollapsed: newState !== undefined ? newState : !state.isCollapsed,
+    }));
+  },
 }));
