ef3edb23d8cf2147e693b521c54ac141f9f7e92b
omniroot <omnirootofc@gmail.com>
2025-07-01 19:51:37 +0300

    Change how collapse working

--- src/components/layouts/global.layout.tsx
+++ src/components/layouts/global.layout.tsx
@@ -1,11 +1,29 @@
-import type { ReactNode } from "react";
 import { Sidebar } from "@/components/business/Sidebar/Sidebar.tsx";
+import { useMedia } from "@/hooks/useMedia.tsx";
+import { useHeader } from "@/stores/header.store.ts";
+
+import { useLocation } from "@tanstack/react-router";
+import { useEffect, type ReactNode } from "react";
 
 export const GlobalLayout = ({ children }: { children: ReactNode }) => {
-	return (
-		<>
-			<Sidebar />
-			<main className="main">{children}</main>
-		</>
-	);
+  const path = useLocation().href;
+  const { isMobile, isTablet } = useMedia();
+  const { toggleIsCollapsed } = useHeader();
+
+  console.log({ isMobile });
+
+  console.log({ path });
+
+  useEffect(() => {
+    if (isMobile || isTablet) {
+      toggleIsCollapsed(true);
+    }
+  }, [path]);
+
+  return (
+    <>
+      <Sidebar />
+      <main className="main">{children}</main>
+    </>
+  );
 };
