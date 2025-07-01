d7f35f9f94862c3346b9e14532730e70b73bc860
omniroot <omnirootofc@gmail.com>
2025-07-01 22:21:24 +0300

    Add chakra cli, use ejected theme

--- src/providers/global.provider.tsx
+++ src/providers/global.provider.tsx
@@ -1,10 +1,10 @@
 import { client } from "@/api/query.client.ts";
 import { ColorModeProvider } from "@/components/ui/color-mode.tsx";
-import theme from "@/components/ui/theme.tsx";
 import { createRouter } from "@/router.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
+import theme from "~/theme/index.ts";
 
 const router = createRouter();
 
