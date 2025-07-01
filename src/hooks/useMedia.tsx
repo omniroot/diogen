09ed1a059e0d65cd8c19090efb34dd20a799a523
omniroot <omnirootofc@gmail.com>
2025-07-01 19:51:59 +0300

    Add useMedia hook

---
+++ src/hooks/useMedia.tsx
@@ -0,0 +1,11 @@
+import { useMediaQuery } from "@chakra-ui/react";
+
+export const useMedia = () => {
+  const [isMobile, isTablet, isDesktop] = useMediaQuery([
+    "max-width: 480px",
+    "max-width: 768px",
+    "max-width: 1280px",
+  ]);
+
+  return { isMobile, isTablet, isDesktop };
+};
