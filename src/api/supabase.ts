98fce4f4743b1a100d5b95d3290606e5126c0e78
omniroot <omnirootofc@gmail.com>
2025-11-21 18:26:28 +0300

    Update supabase api and separate types for diogen and kaizen

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,23 +1,52 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database, Tables, TablesInsert, TablesUpdate } from "@/api/supabase.types";
+import type {
+	Database as DiogenDatabase,
+	Tables as DiogenTables,
+	TablesInsert as DiogenTablesInsert,
+	TablesUpdate as DiogenTablesUpdate,
+} from "@/api/diogen.types.ts";
+import type {
+	Database as KaizenDatabase,
+	Tables as KaizenTables,
+	TablesInsert as KaizenTablesInsert,
+	TablesUpdate as KaizenTablesUpdate,
+} from "@/api/kaizen.types.ts";
 
-export type IIssue = Tables<"issues">;
-export type IIssueInsert = TablesInsert<"issues">;
-export type IIssueUpdate = TablesUpdate<"issues">;
+export type IIssue = DiogenTables<"issues">;
+export type IIssueInsert = DiogenTablesInsert<"issues">;
+export type IIssueUpdate = DiogenTablesUpdate<"issues">;
 
-export type IProject = Tables<"projects">;
-export type IProjectInsert = TablesInsert<"projects">;
-export type IProjectUpdate = TablesUpdate<"projects">;
+export type IProject = DiogenTables<"projects">;
+export type IProjectInsert = DiogenTablesInsert<"projects">;
+export type IProjectUpdate = DiogenTablesUpdate<"projects">;
 
-export type IModule = Tables<"modules">;
-export type IModuleInsert = TablesInsert<"modules">;
-export type IModuleUpdate = TablesUpdate<"modules">;
+export type IModule = DiogenTables<"modules">;
+export type IModuleInsert = DiogenTablesInsert<"modules">;
+export type IModuleUpdate = DiogenTablesUpdate<"modules">;
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export type IHabit = KaizenTables<"habits">;
+export type IHabitInsert = KaizenTablesInsert<"habits">;
+export type IHabitUpdate = KaizenTablesUpdate<"habits">;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-	throw new Error("Supabase URL or Anon Key is missing");
+const diogenSupabaseUrl = import.meta.env.VITE_DIOGEN_SUPABASE_URL;
+const diogenSupabaseAnonKey = import.meta.env.VITE_DIOGEN_SUPABASE_ANON_KEY;
+const kaizenSupabaseUrl = import.meta.env.VITE_KAIZEN_SUPABASE_URL;
+const kaizenSupabaseAnonKey = import.meta.env.VITE_KAIZEN_SUPABASE_ANON_KEY;
+
+if (!diogenSupabaseUrl || !diogenSupabaseAnonKey) {
+	throw new Error("Supabase URL or Anon Key for Diogen is missing");
 }
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+if (!kaizenSupabaseUrl || !kaizenSupabaseAnonKey) {
+	throw new Error("Supabase URL or Anon Key for KAizen is missing");
+}
+
+export const diogen = createClient<DiogenDatabase>(
+	diogenSupabaseUrl,
+	diogenSupabaseAnonKey,
+);
+
+export const kaizen = createClient<KaizenDatabase>(
+	kaizenSupabaseUrl,
+	kaizenSupabaseAnonKey,
+);
