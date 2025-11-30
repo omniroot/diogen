e827f4bd56f68dd3af1bffe4d25b05b9193988d0
omniroot <omnirootofc@gmail.com>
2025-11-30 06:50:06 +0300

    Add some utils for date

---
+++ src/utils/ddate.ts
@@ -0,0 +1,28 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+
+export const ddate = {
+	nowUTC: () => dayjs().utc(),
+
+	toUTC: (input: any) => dayjs(input).utc(),
+	toLocal: (input: any) => dayjs(input).utc().local(),
+
+	// Всегда строка только в ISO (UTC)
+	toDbString: (input: any) => dayjs(input).utc().toISOString(),
+
+	// Строгое отображение локально
+	// toUiString: (input: any) => dayjs(input).local().format("YYYY-MM-DD HH:mm"),
+
+	// Дата сегодня (локально)
+	today: () => dayjs().startOf("day"),
+
+	// Получить YYYY-MM-DD
+	dateKey: (input: any) => dayjs(input).format("YYYY-MM-DD"),
+};
+
+// const today = new Date();
+// console.log(ddate.toUTC(today).toString());
+// console.log(ddate.toLocal(today).toDate());
+// console.log(ddate.toLocal(today).toDate().toString());
