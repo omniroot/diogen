eb71e969f6a77fd654241258a42042211f7df41d
omniroot <omnirootofc@gmail.com>
2025-11-30 06:52:51 +0300

    Add logo to Header

--- src/components/business/Header.tsx
+++ src/components/business/Header.tsx
@@ -1,5 +1,12 @@
-import { HStack, IconButton, Separator } from "@chakra-ui/react";
-import { IconArrowNarrowLeft, IconArrowNarrowRight, IconEdit, IconMenu, IconSearch, IconX } from "@tabler/icons-react";
+import { HStack, Icon, IconButton, Image, Separator } from "@chakra-ui/react";
+import {
+	IconArrowNarrowLeft,
+	IconArrowNarrowRight,
+	IconEdit,
+	IconMenu2,
+	IconSearch,
+	IconX,
+} from "@tabler/icons-react";
 import { useCanGoBack, useRouter } from "@tanstack/react-router";
 import { useMediaQuery } from "@uidotdev/usehooks";
 import { useCanGoForward } from "@/hooks/useCanGoForward.tsx";
@@ -26,26 +33,45 @@ export const Header = () => {
 			// borderColor={"colors.outline"}
 		>
 			<HStack gap="8px" alignItems={"center"}>
-				{isMobile && <IconButton onClick={() => toggleOpen()}>{isOpen ? <IconX /> : <IconMenu />}</IconButton>}
-				<div
+				{isMobile && (
+					<IconButton onClick={() => toggleOpen()}>
+						{isOpen ? <IconX /> : <IconMenu2 />}
+					</IconButton>
+				)}
+				{/* <div
 					style={{
 						width: "32px",
 						height: "32px",
 						backgroundColor: "red",
 						borderRadius: "50%",
 					}}
-				></div>
+				></div> */}
+				<Image w={"32px"} h={"32px"} borderRadius={"md"} src="/logo.png" />
 				<span>Diogen</span>
 			</HStack>
 			<HStack gap="4px" alignItems={"center"}>
-				<IconButton size="sm" variant="ghost" onClick={() => history.back()} disabled={!canGoBack}>
+				<IconButton
+					size="sm"
+					variant="ghost"
+					onClick={() => history.back()}
+					disabled={!canGoBack}
+				>
 					<IconArrowNarrowLeft />
 				</IconButton>
-				<IconButton size="sm" variant="ghost" onClick={() => history.forward()} disabled={!canGoForward}>
+				<IconButton
+					size="sm"
+					variant="ghost"
+					onClick={() => history.forward()}
+					disabled={!canGoForward}
+				>
 					<IconArrowNarrowRight />
 				</IconButton>
 				<Separator orientation="vertical" w={"2px"} h={"20px"} />
-				<IconButton variant="ghost" color={"subtext"} onClick={() => openSearch("create")}>
+				<IconButton
+					variant="ghost"
+					color={"subtext"}
+					onClick={() => openSearch("create")}
+				>
 					<IconSearch />
 				</IconButton>
 				<IconButton onClick={() => openIssue("create")}>
