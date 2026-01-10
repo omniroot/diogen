788f52695184145a5eefb019256914da19a93a32
omniroot <omnirootofc@gmail.com>
2026-01-10 15:04:06 +0300

    feat: add users api

---
+++ src/features/users/api/users.api.ts
@@ -0,0 +1,29 @@
+import { client } from "@/api/api.ts";
+import { tablesDB, usersTable } from "@/api/appwrite.ts";
+import type { Users } from "@/api/types/appwrite.js";
+import { createCoreApi, createHooksApi } from "@/api/utils/appwrite.utils.tsx";
+
+export const usersCoreApi = createCoreApi<Users>({
+	tablesDB: tablesDB,
+	databaseId: usersTable?.databaseId,
+	tableId: usersTable?.$id,
+});
+
+export const { useList: useGetUsers, useOne: useGetUser } = createHooksApi<Users>({
+	coreApis: usersCoreApi,
+	name: "users",
+	queryClient: client,
+});
+
+export function useUsers() {
+	// const {} =
+}
+
+export function useUser(id: string) {
+	const { data: users, isLoading } = useGetUsers(
+		{ user_id: { equal: id } },
+		// { select: (data) => data[0] },
+	);
+
+	return { user: users?.[0], isLoading };
+}
