d3f95717837c387beafd7bc65f35e6040940dc21
omniroot <omnirootofc@gmail.com>
2026-01-30 00:13:28 +0300

    refactor: use getRouter function

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "unfonts.css";
+// import "unfonts.css";
 import { OmnikitProvider } from "@/theme/components/provider.tsx";
 import "./styles/global.css";
 import { client } from "@/api/api.ts";
-import { routeTree } from "~/.tanstack/routeTree.gen.ts";
 import "dayjs/locale/ru";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "@/api/appwrite.ts";
+import { getRouter } from "@/router.tsx";
 
 dayjs.extend(relativeTime);
 dayjs.locale("ru");
 
-const router = createRouter({ routeTree });
+const router = getRouter();
 
 declare module "@tanstack/react-router" {
 	interface Register {
