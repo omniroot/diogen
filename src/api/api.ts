3b58919654fb8c6198e8b48fb8b7ccaca855aa6a
omniroot <omnirootofc@gmail.com>
2026-01-30 23:37:21 +0300

    chore: disable refetch on window focus in api

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,6 +6,7 @@ export const client = new QueryClient({
 			refetchOnReconnect: true,
 			retryDelay: 20000, // Настраиваем задержку перед повторной попыткой (в миллисекундах)
 			staleTime: 5 * 60 * 1000, // Время, в течение которого данные считаются свежими
+			refetchOnWindowFocus: false,
 		},
 	},
 });
